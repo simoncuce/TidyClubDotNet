c6cd4626843c44b070dee19b14937b51b236e9c0
simoncuce <simon@cuce.com.au>
2015-06-29 08:39:09 +1000

    Update AssemblyInfo.cs

--- TidyClubDotNet/Properties/AssemblyInfo.cs
+++ TidyClubDotNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TidyClubDot")]
 [assembly: AssemblyProduct("TidyClubDotNet")]
-[assembly: AssemblyCopyright("Copyright © TidyClub 2015")]
+[assembly: AssemblyCopyright("Copyright © Simon Cuce 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
